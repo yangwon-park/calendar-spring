c0faee6aa5dc687c84a177bdecdea4742f48fe56
yangwon-park <ywonp94@gmail.com>
2025-11-20 09:54:44 +0900

    init project

---
+++ src/main/kotlin/com/calendar/couple/CoupleApplication.kt
@@ -0,0 +1,11 @@
+package com.calendar.couple
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoupleApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoupleApplication>(*args)
+}
---
+++ src/test/kotlin/com/calendar/couple/CoupleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.calendar.couple
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoupleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
